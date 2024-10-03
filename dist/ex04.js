1db4f4905e011488ce9ace0d6aa5b9a8d9f9c6d3
Jorge Ortega <jorgeloscr@hotmail.com>
2024-10-02 23:53:33 -0700

    completed

--- dist/ex04.js
+++ dist/ex04.js
@@ -1,14 +1,13 @@
+"use strict";
 // 4. Create a function named `findMax` that takes an array of numbers as a parameter.
 //    The function should return the largest number in the array.
 //    Ensure the parameter and return type are typed.
-
-function findMax(numbers: number[]): number {
+function findMax(numbers) {
     if (numbers.length === 0) {
         throw new Error("Array should not be empty");
     }
     return Math.max(...numbers);
 }
-
 // Expected output:
 console.log(findMax([1, 5, 3, 9, 2])); // 9
-console.log(findMax([7, 2, 10, 4]));   // 10
+console.log(findMax([7, 2, 10, 4])); // 10
